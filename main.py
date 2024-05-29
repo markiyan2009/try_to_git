f889bf40548c859160be93ac146dfc4834990853
markiyan2009 <andrusikmarkiyan@gmail.com>
2024-05-29 22:23:58 +0300

    update main and requirements, add users.db

--- main.py
+++ main.py
@@ -2,27 +2,28 @@ from fastapi import FastAPI, Query
 from pydantic import BaseModel, Field
 import uvicorn
 from typing import Optional
+import sqlite3
 
 app = FastAPI()
-
+con = sqlite3.connect("users.db")
+cur = con.cursor()
+def get_user_by_id(user_id):
+    cur.execute("SELECT user_name, email FROM users WHERE id=?",user_id)
+    return cur.fetchone()
 class User(BaseModel):
-    id : Optional[int] = Field(primary_key=True)
+    id : int
     username:str
     email:str
 users = [User(id=1,username="ya",email="ya@gmail.com"),User(id=2,username="admin",email="admin@gmail.com")]
 
 @app.post("/users/{user_id}")
 def user_id(user_id:int):
-    
-    for user in users:
-        if user.id == user_id:
-            return user
-    return "User not founded"
+    get_user_by_id(user_id=user_id)
 @app.get("/users")
 def get_users():
     return users
 @app.post("/create_user")
-def create_user(username:str,email:str):
-    user = User(username=username, email=email)
+def create_user(user:User):
+    
     users.append(user)
     return "User was added"
